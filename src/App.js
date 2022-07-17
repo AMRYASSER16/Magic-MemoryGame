6cae5c8eaacbfc0dcd135026a0231052425f106c
Amr Yasser <yamr3212@gmail.com>
2022-07-17 08:21:16 +0200

    adding the logic behind the matched cards

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Card from './components/Card';
 import './App.css';
 
 const cardImages = [
-	{ src: '/img/helmet-1.png' },
-	{ src: '/img/potion-1.png' },
-	{ src: '/img/ring-1.png' },
-	{ src: '/img/scroll-1.png' },
-	{ src: '/img/shield-1.png' },
-	{ src: '/img/sword-1.png' }
+	{ src: '/img/helmet-1.png', matched: false },
+	{ src: '/img/potion-1.png', matched: false },
+	{ src: '/img/ring-1.png', matched: false },
+	{ src: '/img/scroll-1.png', matched: false },
+	{ src: '/img/shield-1.png', matched: false },
+	{ src: '/img/sword-1.png', matched: false }
 ];
 
 function App() {
@@ -28,6 +28,38 @@ function App() {
 
 	const handleChoice = (card) => {
 		choiceOne ? setChoiceTwo(card) : setChoiceOne(card);
+	};
+
+	// comparing 2 choices
+	useEffect(
+		() => {
+			if (choiceOne && choiceTwo) {
+				if (choiceOne.src === choiceTwo.src) {
+					setCards((prevCards) => {
+						return prevCards.map((card) => {
+							if (card.src === choiceOne.src) {
+								return {
+									...card,
+									matched: true
+								};
+							} else {
+								return card
+							}
+						});
+					});
+					resetTurn();
+				} else {
+					resetTurn();
+				}
+			}
+		},
+		[ choiceOne, choiceTwo ]
+	);
+
+	const resetTurn = () => {
+		setChoiceOne(null);
+		setChoiceTwo(null);
+		setTurns((prevTurns) => prevTurns + 1);
 	};
 
 	return (
